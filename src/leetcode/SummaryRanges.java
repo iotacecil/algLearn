2ad0a666b765af6e90063995aeed499240373d96
xiajingCloud <hzxj0730@gmail.com>
2018-07-12 14:12:35 +0800

    7.12 学了一些动态规划的题目from cloud PC

--- src/leetcode/SummaryRanges.java
+++ src/leetcode/SummaryRanges.java
@@ -1,4 +1,11 @@
 package leetcode;
 
 public class SummaryRanges {
+    Integer iitt;
+    int zero ;
+    public static void main(String[] args) {
+        SummaryRanges sr = new SummaryRanges();
+        System.out.println(sr.iitt);
+        System.out.println(sr.zero );
+    }
 }
